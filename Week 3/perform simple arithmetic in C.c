62b631c9a32c8c015e67fcd29fe88330f8e88c36
Kareem Osama Bahnasy <35812858+kareembahnasy@users.noreply.github.com>
2023-10-01 02:11:39 +0300

    Update perform simple arithmetic in C.c

--- Week 3/perform simple arithmetic in C.c
+++ Week 3/perform simple arithmetic in C.c
@@ -5,7 +5,7 @@
     int y = 60*24*x ;
     int z = 60*y;
     printf("Dear Procrastinator,\n");
-    printf("You still have to wait for %d days %d minutes or %d seconds before you can procrastinate!",x,y,z) ;
+    printf("You still have to wait for %d days (%d minutes or %d seconds) before you can procrastinate!",x,y,z) ;
 
     return(0);
  }
