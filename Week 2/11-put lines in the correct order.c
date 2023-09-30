9670e4dba1b0ad9dd7226ee513761d7b5598c2c7
Kareem Osama Bahnasy <35812858+kareembahnasy@users.noreply.github.com>
2023-09-30 03:54:17 +0300

    Create 11-put lines in the correct order.c

---
+++ Week 2/11-put lines in the correct order.c
@@ -0,0 +1,18 @@
+ //preprocessor directive
+ #include <stdio.h>
+ // main function
+ int main(void){
+    //variable declaration
+    int i;
+    printf("I ");
+    //executable statement
+      for(i = 0; i < 3 ; i++) {
+        printf("love ");
+      }
+      printf("programming!");
+      // return statement
+      return(0);
+
+    
+
+    }
