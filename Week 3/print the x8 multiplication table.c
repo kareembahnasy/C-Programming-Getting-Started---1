5db2958e99d4378c5af056d53e8ece7bdebdb9c8
Kareem Osama Bahnasy <35812858+kareembahnasy@users.noreply.github.com>
2023-10-01 04:15:29 +0300

    Create print the x8 multiplication table.c

---
+++ Week 3/print the x8 multiplication table.c
@@ -0,0 +1,9 @@
+int main(){
+      int multiplier = 8;
+      for (int i = 0 ; i <= 10; i++){
+        int result = multiplier * i;
+        printf("%d x %d = %d \n", multiplier ,i , result);
+      }
+      
+    return 0;
+ }
